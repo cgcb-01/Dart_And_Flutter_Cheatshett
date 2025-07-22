d32117788d95921a0f29d50d3acfb9a0536c1f72
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-07-22 23:29:35 +0530

    Working wit te ui of the app

--- demo_app_learning/lib/Pages/home.dart
+++ demo_app_learning/lib/Pages/home.dart
@@ -1,1 +1,21 @@
 import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Attendance Tracker',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}

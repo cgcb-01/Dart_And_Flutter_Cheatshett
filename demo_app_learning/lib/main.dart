5a6811434f2231298957ef3daf39cf1129d40c03
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-07-23 00:14:36 +0530

    Changes in te calendar features

--- demo_app_learning/lib/main.dart
+++ demo_app_learning/lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+import './Pages/home.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(),
+    ),
+  );
+}

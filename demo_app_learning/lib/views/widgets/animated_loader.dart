e24fc066fcfd1b07027971857681026c0dffa2dd
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-07-30 00:24:26 +0530

    animated loader

---
+++ demo_app_learning/lib/views/widgets/animated_loader.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class AnimatedLoader extends StatefulWidget {
+  const AnimatedLoader({super.key});
+
+  @override
+  State<AnimatedLoader> createState() => _AnimatedLoaderState();
+}
+
+class _AnimatedLoaderState extends State<AnimatedLoader>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(
+      begin: 0.8,
+      end: 1.2,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: _animation.value,
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 10,
+                  spreadRadius: 2,
+                ),
+              ],
+            ),
+            child: const Icon(Icons.school, size: 60, color: Colors.deepPurple),
+          ),
+        );
+      },
+    );
+  }
+}
